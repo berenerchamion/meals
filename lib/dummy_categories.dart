33a6ec93c11ddb037dfd1a9ff2dc6b9c2c2c59eb
Beren Erchamion <beren@houseofbeor.net>
2021-01-10 16:51:12 -0500

    Working on categories

---
+++ lib/dummy_categories.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import './models/category.dart';
+
+const DUMMY_CATEGORIES = const [
+  Category(
+    id: 'c1',
+    title: 'Italian',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'c2',
+    title: 'Chinese',
+    color: Colors.lightBlue,
+  ),
+  Category(
+    id: 'c3',
+    title: 'American',
+    color: Colors.red,
+  ),
+  Category(
+    id: 'c4',
+    title: 'French',
+    color: Colors.orange,
+  ),
+  Category(
+    id: 'c5',
+    title: 'Scottish',
+    color: Colors.blueGrey,
+  ),
+  Category(
+    id: 'c6',
+    title: 'Mexican',
+    color: Colors.yellow,
+  ),
+  Category(
+    id: 'c7',
+    title: 'Pizza',
+    color: Colors.orange,
+  ),
+  Category(
+    id: 'c7',
+    title: 'Hamburgers',
+    color: Colors.brown,
+  ),
+];
--- lib/widgets/categories_screen.dart
+++ lib/widgets/categories_screen.dart
@@ -5,8 +5,14 @@ class CategoriesScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return GridView(
       children: <Widget>[
-        
+
       ],
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 /2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
+      ),
     );
   }
 }
