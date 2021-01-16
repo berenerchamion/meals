b4c92c5458baeff3a21a9a601c621484e5946f3d
Beren Erchamion <beren@houseofbeor.net>
2021-01-16 16:00:43 -0500

    I have a new screen and navigation

--- lib/widgets/category_item.dart
+++ lib/widgets/category_item.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+import './category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   final String title;
@@ -6,24 +9,35 @@ class CategoryItem extends StatelessWidget {
 
   CategoryItem(this.title, this.color);
 
+  void selectCategory(BuildContext ctx) {
+    Navigator.of(ctx).push(MaterialPageRoute(builder: (_) {
+      return CategoryMealsScreen();
+    },),);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            color.withOpacity(0.7),
-            color,
-          ],
-          begin: Alignment.centerLeft,
-          end: Alignment.centerRight,
-        ), //LinearGradient
-        borderRadius: BorderRadius.circular(15),
-      ), //BoxDecoration
-      child: Text(
-        title,
-        style: Theme.of(context).textTheme.headline6,
+    return InkWell(
+      onTap: () => selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              color.withOpacity(0.7),
+              color,
+            ],
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
+          ), //LinearGradient
+          borderRadius: BorderRadius.circular(15),
+        ), //BoxDecoration
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.headline6,
+        ),
       ),
     );
   }
--- lib/widgets/category_meals_screen.dart
+++ lib/widgets/category_meals_screen.dart
@@ -3,6 +3,13 @@ import 'package:flutter/material.dart';
 class CategoryMealsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('The Recipies'),
+      ),
+      body: Center(
+        child: Text('Here are the recipies for the category.'),
+      ),
+    );
   }
 }
